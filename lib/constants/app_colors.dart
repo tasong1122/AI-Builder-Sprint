3f41a208f78628a3d746f1472471e1164800f762
jisoo1024 <jisoo10241@naver.com>
2026-07-31 21:55:18 +0900

    색 변경

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -4,7 +4,7 @@ abstract final class AppColors {
   static const Color background = Color(0xFFFFFFFF);
   static const Color sectionBackground = Color(0x80A0EAF7);
   static const Color neutralSection = Color(0x33787878);
-  static const Color highlight = Color(0xFF48E5FD);
+  static const Color highlight = Color(0xFF95D7FA);
   static const Color darkHighlight = Color(0xFF95D7FA);
   static const Color textPrimary = Color(0xFF111111);
   static const Color textSecondary = Color(0xFF787878);
